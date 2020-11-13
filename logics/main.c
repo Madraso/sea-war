14bf6c1eb7c8d22baa1024cf2e3daac31cf99995
seawar2020-november <74137809+seawar2020-november@users.noreply.github.com>
2020-11-13 11:38:50 +0700

    Add files via upload

--- logics/main.c
+++ logics/main.c
@@ -43,7 +43,7 @@ struct player{
 };
 
 
-void save_field(struct player *player){
+void save_field(struct player *player){//сохранение поля игрока
 FILE *fd;
 fd=fopen(player->path,"r+");
 for(int i=0;i<FIELD_SIZE;i++){
@@ -54,7 +54,7 @@ fprintf(fd,"%c",player->field[i][j]);
 }
 pclose(fd);
 }
-void open_field(struct player *player){
+void open_field(struct player *player){//открытие поля игрока
 FILE *fd;
 fd=fopen(player->path,"r");
 for(int i=0;i<FIELD_SIZE;i++){
@@ -63,18 +63,18 @@ fscanf(fd,"%c",&player->field[i][j]);
 	}
 }
 }
-void drow_ship(struct player *player){
-	for(int k=player->current_ship;k>=0;k--){
-	if(player->boat[player->current_ship].orientation==vertical){
-	for(int i=player->boat[player->current_ship].start_y;i<player->boat[player->current_ship].start_y+player->boat[player->current_ship].length;i++){
-	player->field[i][player->boat[player->current_ship].start_x]='0';	
+void drow_ship(struct player *player){//отрисовка кораблей
+	for(int k=9;k>=player->current_ship;k--){
+	if(player->boat[k].orientation==vertical){
+	for(int i=player->boat[k].start_y;i<player->boat[k].start_y+player->boat[k].length;i++){
+	player->field[i][player->boat[k].start_x]='0';	
 	
 	}
 }
-else{
+	if(player->boat[k].orientation==gorisont){
 
-	for(int i=player->boat[player->current_ship].start_x;i<player->boat[player->current_ship].start_x+player->boat[player->current_ship].length;i++){
-	player->field[player->boat[player->current_ship].start_y][i]='0';	
+	for(int i=player->boat[k].start_x;i<player->boat[k].start_x+player->boat[k].length;i++){
+	player->field[player->boat[k].start_y][i]='0';	
 	
 	}
 
@@ -83,19 +83,19 @@ else{
 }
 
 }
-void init_map(void* param){
+void init_map(void* param){// создание карты и отрисовка кораблей
 	struct player *player=(struct player*) param;
-
+		open_field(player);	
+	
 	for(int i =0;i<FIELD_SIZE;i++){
 		for(int j=0;j<FIELD_SIZE;j++){
-		if(player->field[i][j]!='0'){
+	//	if(player->field[i][j]!='0'){
 		player->field[i][j]=' ';
-		}
-		else{
+	//	}
+		/*else{
 		player->field[i][j]='0';
-		}	
-		if((i==player->boat[player->current_ship].start_y)&&(j==player->boat[player->current_ship].start_x)){
-			//drow_ship(player);
+		}*/	
+		if(((i==player->boat[player->current_ship].start_y)&&(j==player->boat[player->current_ship].start_x))&&(player->field[i][j]!='0')){
 			player->field[i][j]='+';
 			}
 		
@@ -115,41 +115,41 @@ void init_map(void* param){
 		else if(j==FIELD_SIZE-1){
 			player->field[i][j]='#';	
 			}
+		drow_ship(player);
 		printf("%c",player->field[i][j]);	
 		}
 			printf("\n");
 	}	
-
+save_field(player);
 }
-void init_map_other_player(struct player *player,struct aim *aim){
+void init_map_other_player(struct player *player,struct aim *aim){//игровое поле, где игрок стреляет
 	open_field(player);
+	char field1[FIELD_SIZE][FIELD_SIZE];
 	for(int i =0;i<FIELD_SIZE;i++){
 		for(int j=0;j<FIELD_SIZE;j++){
-		if(player->field[i][j]!='*'){
-		player->field[i][j]=' ';
+		field1[i][j]=' ';
+		if(player->field[aim->y][aim->x]=='0'){
+		field1[aim->y][aim->x]='*';
 		}
-		else{
-		player->field[i][j]='*';
-		}	
 		if((i==aim->y)&&(j==aim->x)){
-			player->field[i][j]='+';
+		field1[i][j]='+';
 			}
 		
 		else if(i==0){
-			player->field[i][j]='#';	
+		field1[i][j]='#';	
 		}
 		
 		else if(i==FIELD_SIZE-1){
-			player->field[i][j]='#';	
+		field1[i][j]='#';	
 		}
 	
 		
 		else if(j==0){
-			player->field[i][j]='#';	
+		field1[i][j]='#';	
 		}
 		
 		else if(j==FIELD_SIZE-1){
-			player->field[i][j]='#';	
+		field1[i][j]='#';	
 			}
 		
 		printf("%c",player->field[i][j]);	
@@ -158,52 +158,64 @@ void init_map_other_player(struct player *player,struct aim *aim){
 	}	
 
 }
-
+//	
+//
+//     xxxxxx
+//     x0000x
+//     xxxxxx
+//     xxx
+//     x0x
+//     x0x
+//     x0x
+//     xxx
 //проверки можем ли мы расположить корабль
-bool check_place(struct player *player){
-	for(int i=9;i>=0;i--){
-		
-	int l=player->boat[i].length;
-		for(int j=0;j<l;j++){
-			if(player->boat[player->current_ship].orientation==vertical){
-				if(((player->boat[player->current_ship].start_x!=player->boat[i].start_x+l)&&
-				(player->boat[player->current_ship].start_y!=player->boat[i].start_y))||
-				((player->boat[player->current_ship].start_x!=player->boat[i].start_x+l)||	
-			  	(player->boat[player->current_ship].start_x!=player->boat[i].start_x-1))&&
-			 	((player->boat[player->current_ship].start_y!=player->boat[i].start_y+1)||
-			  	(player->boat[player->current_ship].start_y!=player->boat[i].start_y-1))){
-		drow_ship(player);	
-		return true;	
+bool check_place(struct player *player){									
+	int i=player->current_ship;
+	int b=player->boat[player->current_ship].length;	
+	bool still[1][b+1];
+		for(int k=0;k<2;k++){
+		for(int j=0;j<=b+1;j++){
+			if(player->boat[i].orientation==gorisont){
+			if((player->field[player->boat[i].start_y+1][player->boat[i].start_x+(j-1)]==' ')&&
+				(player->field[player->boat[i].start_y-1][player->boat[i].start_x+(j-1)]==' ')){
+			
+			
+						
+			still[k][j]=true;						
 			}
 	else{
-	return false;
+	still[k][j]=false;
+	
 	}
 			}
-			else{
-			
-				if(((player->boat[player->current_ship].start_x!=player->boat[i].start_x)&&
-				(player->boat[player->current_ship].start_y!=player->boat[i].start_y+l))||
-				((player->boat[player->current_ship].start_x!=player->boat[i].start_x+1)||	
-			  	(player->boat[player->current_ship].start_x!=player->boat[i].start_x-1))&&
-			 	((player->boat[player->current_ship].start_y!=player->boat[i].start_y+l)||
-			  	(player->boat[player->current_ship].start_y!=player->boat[i].start_y-1))){
-			drow_ship(player);
-			return true;	
+			if(player->boat[i].orientation==vertical){
+			if((player->field[player->boat[i].start_y+(j-1)][player->boat[i].start_x-1]==' ')&&
+			(player->field[player->boat[i].start_y+(j-1)][player->boat[i].start_x+1]==' ')){
+			still[k][j]=true;						
 			}
 	else{
-	return false;
+	still[k][j]=false;
+	
 	}
-			
-			
-			}
 		
-		}
+}}		
 }
+bool l=true;
+for(int k=0;k<2;k++)	{
+		for(int j=0;j<=b+1;j++){
+	l= l && still[k][j];	
+	
+	
+	}
+		}
+	
+return l;
 }
 /*
 void check_final_place(struct player *player){
 	if(player->)	
 	
+				((player->boat[i].orientation==vertical)&&(player->field[player->boat[i].start_y+j][player->boat[i].start_x]!='0'))){
 
 }*/
 
@@ -235,18 +247,18 @@ int p=player->current_ship;
 			printf("4palub\n");
 
 }
-void placement(void *param){
+void placement(void *param){//расстановка короблей
 struct player *p = (struct player*) param;
 int x_size=FIELD_SIZE;//размеры поля по х
 int y_size=FIELD_SIZE;//размеры поля по у
-p->current_ship=0;
+p->current_ship=9;
 bool still_works=true;
+init_map((struct player*)p);
 for(int k=0;k<10;k++){
 p->boat[k].orientation=gorisont;
 p->boat[k].start_x=FIELD_SIZE/2;
 p->boat[k].start_y=FIELD_SIZE/2;
 }
-init_map((struct player*)p);
 while(still_works){
 	print_numship(p);
 	switch(getc(stdin)){//задание начальных точек
@@ -315,14 +327,14 @@ while(still_works){
 		break;
 	case 'x':		
 		if(check_place(p)){
-		if(p->current_ship!=10){
-		p->current_ship++;
-		save_field(p);
-		init_map((struct player*)p);
-		}
-		else{
-		still_works=false;
-		}
+			if(p->current_ship>=0){
+			p->current_ship--;
+			save_field(p);
+			init_map((struct player*)p);
+			}
+			else{
+			still_works=false;
+			}
 		}
 		else{
 		printf("anather place\n");
@@ -361,33 +373,33 @@ return false;
 }
 
 }
-void gun(struct player *player,struct aim *aim){
+void shoot(struct aim *aim){
 
-open_field(player);
-init_map_other_player(player,aim);
-aim->y=5;
-aim->x=5;
 printf("Enter place\n");
 
 scanf("%d",&aim->y);
 scanf("%d",&aim->x);
+}
 
-if(player->field[aim->y][aim->x]=='0'){
-printf("you popal");
-player->health--;
-player->field[aim->y][aim->x]='*';
-save_field(player);
+void popal_or_nepopal(struct player *player,struct aim *aim){
+shoot(aim);
+init_map_other_player(player,aim);
+if(player->field[aim->y][aim->x]!='0'){
+printf("nepopal\n");
+//player->health--;
 }
 else{
-printf("miss");
-
+printf("popal\n");
+player->health--;
 }
+
 }
 
 int main(int argc,char *argv[]){
 struct player *player1=(struct player*)calloc(1,sizeof(struct player));
 struct player *player2=(struct player*)calloc(1,sizeof(struct player));
-struct aim *aim=(struct aim*)calloc(1,sizeof(struct aim));
+struct aim *aim1=(struct aim*)calloc(1,sizeof(struct aim));
+struct aim *aim2=(struct aim*)calloc(1,sizeof(struct aim));
 player1->path="player1_field.txt";
 player2->path="player2_field.txt";
 player1->health=20;
@@ -411,7 +423,11 @@ player1->boat[8].length=3;
 player1->boat[9].length=4;
 placement((struct player* )player1);
 while(check_finish(player1,player2)){
-gun(player2,aim);
+popal_or_nepopal(player1,aim2);
 }
+free(player1);
+free(player2);
+free(aim1);
+free(aim2);
 return 0;
        }
